8db52d651a190d248c853ad5f6f32c3105d82620
Dishari Roy <68069424+dishariroy@users.noreply.github.com>
2021-03-09 13:05:32 +0530

    Create speakhello.js

---
+++ module4/speakhello.js
@@ -0,0 +1,9 @@
+(function(window) {
+    var helloSpeaker = new Object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
+© 2020 GitHub, Inc.
